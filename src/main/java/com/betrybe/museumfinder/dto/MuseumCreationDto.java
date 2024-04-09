b0122d71c5fdc23c068f002f5ef202b417c32a4f
Anderson-Reges <andersonreges76@gmail.com>
2024-04-09 15:12:06 -0300

    feat: creating the Museum class and its DTOS

---
+++ src/main/java/com/betrybe/museumfinder/dto/MuseumCreationDto.java
@@ -0,0 +1,19 @@
+package com.betrybe.museumfinder.dto;
+
+import com.betrybe.museumfinder.model.Coordinate;
+
+/**
+ * This record representing a Dto the creation museum class.
+ */
+public record MuseumCreationDto(
+    Long id,
+    String name,
+    String description,
+    String address,
+    String collectionType,
+    String subject,
+    String url,
+    Coordinate coordinate
+) {
+
+}
